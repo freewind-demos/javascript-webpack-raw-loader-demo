32f0c0b91ae69984a1ba57a92dd47ddd2c9b5b73
freewind <nowindlee@gmail.com>
2018-09-21 19:49:21 +0800

    demo

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ JavaScript Webpack "raw-loader" Demo
 
 在JavaScript中，把指定的文件以字符串的形式import进来。
 
+需要注意的是，webpack对`.json`文件有特殊的处理，无法使用外部loader，只能以object的形式自动加载。
+
 ```
 npm install
 npm run demo
--- entry.js
+++ entry.js
@@ -1,6 +1,9 @@
 const data = require('./data.txt')
-const css = require('raw-loader!./index.css')
+const css = require('raw-loader!./data.css')
+const json = require('./data.json')
 
 document.write(data)
 document.write('<hr/>')
 document.write(css)
+document.write('<hr/>')
+document.write(json)
